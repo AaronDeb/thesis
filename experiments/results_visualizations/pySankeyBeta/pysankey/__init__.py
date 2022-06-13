070d4cee13c69a704ded7ca04311b7bb923b8ada
AaronDeb <aaron.deb.31@gmail.com>
2022-06-13 22:36:28 +0200

    Remove unneeded files

--- experiments/results_visualizations/pySankeyBeta/pysankey/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Make simple, pretty Sankey Diagrams """
-
-# from pysankey.sankey import LabelMismatch, NullsInFrame, PySankeyException, sankey
-
-__all__ = ["sankey", "PySankeyException", "NullsInFrame", "LabelMismatch"]
